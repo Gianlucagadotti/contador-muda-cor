83a6edb3a9f0d49985665f1c1abf6a286f9a7e20
Gianlucagadotti <gianlucagadotti@gmail.com>
2025-04-12 17:59:09 -0300

    v-model-para-escolher-cor

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,5 @@
 <script setup>
-
-import { computed, ref } from "vue";
-
+import { computed, ref } from 'vue'
 
 const contador = ref(0)
 
@@ -18,54 +16,50 @@ const mudarcor = [
     id: 2,
     cor: 'branco',
   },
-];
+]
 
 function incrementar(variavel) {
-
   eval(variavel).value++
 }
-function decrementar(variavel){
+function decrementar(variavel) {
   eval(variavel).value--
 }
-function incrementarComParametros(variavel) {
-
-    if(eval(variavel).value <= 3){
-
-      eval(variavel).value++
-    }
-    else{
-      eval(variavel).value = 0
-    }
-}
- function decrementarComParemetros(variavel) {
-
-  if(eval(variavel).value > 0){
-
-eval(variavel).value--
-}
-
-  else{
-  eval(variavel).value = 0
-  }
- }
 </script>
 
 <template>
-  <div>
+  <div class="app">
+    <label for="v-model">Escolher cor: </label>
+    <select v-model="selectedColor">
+      <option value="red">Vermelho</option>
+      <option value="green">Verde</option>
+      <option value="blue">Azul</option>
+    </select>
 
+<div>
+    <button @click="incrementar('contador')">incrementar +</button>
+    <button @click="decrementar('contador')">decrementar -</button>
 
-  <button @click="incrementar('contador')">incrementar</button>
-
-<p :style="{ backgroundColor: contador2 == 1 ? 'green' : contador2 == 2 ? 'orange' : contador2 == 3 ? 'pink' : contador2 == 4 ? 'yellow' : 'black' }">{{ contador }}</p>
-
+    <p :style="{ backgroundColor: selectedColor }" class="color">{{ contador }}</p>
+</div>
   </div>
-  <button @click="incrementarComParametros('contador2')">mudar cor +</button>
-  <button @click="decrementarComParemetros('contador2')">mudar cor -</button>
-  <p>{{ contador2 }}</p>
-
 </template>
 
 <style scoped>
+button {
+  background-color: black;
+  color: white;
 
+}
+select{
+  background-color: black;
+  color: white;
 
+}
+ option{
+  background-color: black;
+  color: white;
+ }
+ p{
+  color: black;
+ }
 </style>
